d7ac1d008052a5c3792cae0b5f9497ebaaa5f29f
leokenta <leonardo.kenta02@gmail.com>
2025-02-21 17:03:18 -0300

    Mudei o nome

--- aula/src/main/java/org/example/exercicio1/Main.java
+++ aula/src/main/java/org/example/exercicio1/Main.java
@@ -2,7 +2,7 @@ package org.example.exercicio1;
 public class Main {
     public static void main(String[] args) {
 
-    Pessoa pessoa = new Pessoa( "leo", 20); // acrescentando valores nos contrutores
+    Pessoa pessoa = new Pessoa( "Andreo", 34); // acrescentando valores nos contrutores
     pessoa.buscarInformacoes(); //aplicando o metodo que foi criado na classe (Pessoa)
 
         }
